6c1fb6426d5b1c163225cfe28c706d5ff61535aa
Michael Missos <michmissos@gmail.com>
2026-09-09 18:57:35 +0300

    pipeline

---
+++ 01-docker-sql/pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+print('arguments', sys.argv)
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1,2], "num_passngers": [3,4]})
+df['month'] = month
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+print(df.head())
+print(f'hello pipeline,month={month}')
